d7c817afb907c4d9f0e48ebaf1a6ed55ef074b53
Pedro Ivo Kuhn <kuhnpedroivo@gmail.com>
2023-10-05 19:20:58 -0300

    Project Created

---
+++ src/test/java/com/pedro/caixa/CaixaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.caixa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaixaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
